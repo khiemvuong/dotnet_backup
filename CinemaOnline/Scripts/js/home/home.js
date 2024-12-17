f2c4f6474f5157342704aff847a894bb49b50ae3
Marethy <khiemmm2004@gmail.com>
2024-12-17 16:06:40 +0700

    fine

---
+++ CinemaOnline/Scripts/js/home/home.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#search').click(function () {
+        $('#formSearch').submit();
+    });
+
+    $('#formSearch').submit(function (e) {
+        e.preventDefault();
+
+        utils.loading();
+
+        const filter = $("#slFilter").val();
+        const keywords = $("#keywords").val();
+        let url = filter === "1" ? `/Film/Search?keyword=${keywords}` : `/Film/Search?${filter}=${keywords}`;
+
+        // Redirect to the search URL
+        window.location.href = url;
+    });
+});
